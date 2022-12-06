dbfc6caed8822f102fa86f935626a826fe208b0d
Harry <hcodu37@gmail.com>
2022-12-05 20:23:43 -0500

    Working in case v1

--- code.py
+++ code.py
@@ -5,9 +5,8 @@ import wifi
 import socketpool
 import board
 import busio
-import adafruit_requests
-from digitalio import DigitalInOut
 import neopixel
+from digitalio import DigitalInOut
 
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi
@@ -16,8 +15,10 @@ from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_io.adafruit_io import IO_MQTT
 
+# ----- Global state variables
 LIGHTS_ON = True
 MODE = "bmw"
+
 
 try:
     from secrets import secrets
@@ -25,31 +26,27 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
+# ----- NeoPixel setup
+ORDER = neopixel.GRB
 pixel_pin = board.GP0
 num_pixels = 180
-ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels)    # Pico Wiring
+pixels.brightness = 0.2
 
-pixels = neopixel.NeoPixel(board.GP0, 180)    # Feather wiring!
-pixels.brightness = 0.1
-
-
-
+# ----- Socketpool, esp, and spi setup
 pool = socketpool.SocketPool(wifi.radio)
-
+spi = busio.SPI(board.GP10, board.GP11, board.GP12)
 esp32_cs = DigitalInOut(board.GP13)
 esp32_ready = DigitalInOut(board.GP14)
 esp32_reset = DigitalInOut(board.GP15)
-
-spi = busio.SPI(board.GP10, board.GP11, board.GP12)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
-print("Connecting to wifi!")
-
-# wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
-wifi.radio.connect(os.getenv('WIFI_SSID'), os.getenv('WIFI_PASSWORD'))
+# ----- Connects to WIFI and prints ipv4 address
+print("\n Connecting to wifi!")
+wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("My IP address is", wifi.radio.ipv4_address)
 
-
+# ----- Methods for handling Adafruit IO MQTT traffic
 def connected(client):
     # Connected function will be called when the client is connected to Adafruit IO.
     # This is a good place to subscribe to feed changes.  The client parameter
@@ -81,17 +78,14 @@ def on_message(client, feed_id, payload):
     
     if feed_id == "pico-mode-feed":
         MODE = payload
+        
+    if feed_id == "pico-brightness-feed":
+        pixels.brightness = int(payload) / 100
+        print("TESTTT")
     
     if LIGHTS_ON:
         if MODE == "bmw":
-            for i in range(22):
-                pixels[i] = (0, 170, 255)
-            for i in range(22):
-                pixels[i + 22] = (0, 0, 255)
-            for i in range(22):
-                pixels[i + 44] = (255, 0, 0)
-            for i in range(60):
-                pixels[i + 66] = (255, 255, 255)  
+            bmw_colors()
         if MODE == "rgb":
             rainbow_cycle(0.0001)
 
@@ -101,32 +95,32 @@ def on_message(client, feed_id, payload):
            
     print("Feed {0} received new value: {1}".format(feed_id, payload))
 
-
+# ----- Setup and initalize MQTT and Adafruit IO MQTT 
 MQTT.set_socket(socket, esp)
-
 mqtt_client = MQTT.MQTT(
     broker="io.adafruit.com",
     port=1883,
-    username=os.getenv('aio_username'),
-    password=os.getenv('aio_key'),
+    username=secrets['aio_username'],
+    password=secrets['aio_key'],
     socket_pool = pool,
     ssl_context = ssl.create_default_context(),
 )
-
 io = IO_MQTT(mqtt_client)
 
+# ----- Pass IO methods to client
 io.on_connect = connected
 io.on_disconnect = disconnected
 io.on_subscribe = subscribe
 io.on_unsubscribe = unsubscribe
 io.on_message = on_message
 
+# ----- Connect to Adafruit IO MQTT server, subscribe to 'pico feeed' and 'pico-mode-feed' 
 io.connect()
-
-
 io.subscribe('pico-feeed')
 io.subscribe('pico-mode-feed')
+io.subscribe('pico-brightness-feed')
 
+# ----- Methods for doing a rainbow wave RGB effect and BMW RGB Effect
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
@@ -150,27 +144,36 @@ def wheel(pos):
 def rainbow_cycle(wait):
     for j in range(255):
         # print("J + " + str(j))
-        for i in range(30):
+        for i in range(180):
             # print("I + " + str(i))
             pixel_index = (i * 256 // num_pixels) + j
             pixels[i] = wheel(pixel_index & 255)
         pixels.show()
     print("FINISHED")
+def bmw_colors():
+    for i in range(57):
+        pixels[i] = (255, 255, 255)
+        time.sleep(0.03)
+    for i in range(40):
+        pixels[i + 57] = (255, 0, 0)
+        time.sleep(0.03)
+    for i in range(40):
+        pixels[i + 97] = (0,0,255)
+    for i in range(40):
+        pixels[i + 137] = (0,150,255)
+
+bmw_colors()
 
 while True:    
     try:
         io.loop()
-        # if MODE == "rgb" and LIGHTS_ON:
-        #     rainbow_cycle(0)
     except (ValueError, RuntimeError, Exception) as e:
-
-        print("Failed to loop MQTT, reconnecting \n", e)
+        print("Failed to loop MQTT, attempting to reconnect \n", e)
 
         try:
             io.reconnect()
         except(ValueError, RuntimeError, Exception) as e:
-            print("Failed to reconnect MQTT, reconnecting \n", e)
-            io.publish("error-feed", e)
+            print("Failed to reconnect to MQTT, reconnecting \n", e)
     
     print("MODE: " + MODE + " ON?: " + str(LIGHTS_ON))
     # time.sleep(1)
