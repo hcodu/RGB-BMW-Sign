9089b1c662715ccb9358ebb831d043b42fed43c8
Harry <hcodu37@gmail.com>
2022-12-11 14:56:28 -0500

    Added comet, rgb, and pulse animations

--- code.py
+++ code.py
@@ -8,6 +8,17 @@ import busio
 import neopixel
 from digitalio import DigitalInOut
 
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.animation.customcolorchase import CustomColorChase
+from adafruit_led_animation.animation.Rainbow import Rainbow
+
+
+from adafruit_led_animation.color import RED, BLUE, WHITE
+
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_esp32spi import adafruit_esp32spi_wifimanager
@@ -18,7 +29,10 @@ from adafruit_io.adafruit_io import IO_MQTT
 # ----- Global state variables
 LIGHTS_ON = True
 MODE = "bmw"
-
+M_END_PIXEL_INDEX = 53
+SLASH1_END_PIXEL_INDEX = 81
+SLASH2_END_PIXEL_INDEX = 106
+SLASH3_END_PIXEL_INDEX = 128
 
 try:
     from secrets import secrets
@@ -30,8 +44,25 @@ except ImportError:
 ORDER = neopixel.GRB
 pixel_pin = board.GP0
 num_pixels = 180
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels)    # Pico Wiring
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write = False)    # Pico Wiring
 pixels.brightness = 0.2
+
+pulse_red = Pulse(pixels, speed=0.1, period=4, color=RED)
+pulse_blue = Pulse(pixels, speed=0.1, period=4, color=BLUE)
+pulse_white = Pulse(pixels, speed=0.1, period=4, color=WHITE)
+
+comet_red = Comet(pixels, speed=0.05, color=RED, tail_length=20, ring = True)
+comet_blue = Comet(pixels, speed=0.05, color=BLUE, tail_length=20, ring = True)
+comet_white = Comet(pixels, speed=0.05, color=WHITE, tail_length=20, ring = True)
+
+comet_animations = AnimationSequence(
+    comet_red, comet_blue, comet_white, advance_interval=9.1, auto_clear=True, auto_reset = True
+)
+pulse_animations = AnimationSequence(
+    pulse_red, pulse_blue, pulse_white, advance_interval = 4, auto_clear=False, auto_reset = True,
+)
+
+rainbow = Rainbow(pixels, speed=0.1, period=5, step=0.5)
 
 # ----- Socketpool, esp, and spi setup
 pool = socketpool.SocketPool(wifi.radio)
@@ -59,11 +90,9 @@ def subscribe(client, userdata, topic, granted_qos):
 def unsubscribe(client, userdata, topic, pid):
     # This method is called when the client unsubscribes from a feed.
     print("Unsubscribed from {0} with PID {1}".format(topic, pid))
-# pylint: disable=unused-argument
 def disconnected(client):
     # Disconnected function will be called when the client disconnects.
     print("Disconnected from Adafruit IO!")
-# pylint: disable=unused-argument
 def on_message(client, feed_id, payload):
     # Message function will be called when a subscribed feed has a new value.
     # The feed_id parameter identifies the feed, and the payload parameter has
@@ -85,12 +114,11 @@ def on_message(client, feed_id, payload):
     
     if LIGHTS_ON:
         if MODE == "bmw":
-            bmw_colors()
-        if MODE == "rgb":
-            rainbow_cycle(0.0001)
+            bmw_colors()      
 
     if not LIGHTS_ON:
         pixels.fill((0, 0, 0))
+        pixels.show()
     
            
     print("Feed {0} received new value: {1}".format(feed_id, payload))
@@ -114,66 +142,49 @@ io.on_subscribe = subscribe
 io.on_unsubscribe = unsubscribe
 io.on_message = on_message
 
-# ----- Connect to Adafruit IO MQTT server, subscribe to 'pico feeed' and 'pico-mode-feed' 
+# ----- Connect to Adafruit IO MQTT server, subscribe to 'pico-feeed', 'pico-mode-feed', and 'pico-brightness-feed' 
 io.connect()
 io.subscribe('pico-feeed')
 io.subscribe('pico-mode-feed')
 io.subscribe('pico-brightness-feed')
 
-# ----- Methods for doing a rainbow wave RGB effect and BMW RGB Effect
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-def rainbow_cycle(wait):
-    for j in range(255):
-        # print("J + " + str(j))
-        for i in range(180):
-            # print("I + " + str(i))
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-    print("FINISHED")
-def bmw_colors():
-    for i in range(57):
-        pixels[i] = (255, 255, 255)
-        time.sleep(0.03)
-    for i in range(40):
-        pixels[i + 57] = (255, 0, 0)
-        time.sleep(0.03)
-    for i in range(40):
-        pixels[i + 97] = (0,0,255)
-    for i in range(40):
-        pixels[i + 137] = (0,150,255)
 
+def bmw_colors():
+    pixels.fill((0,0,0))
+    for i in range(M_END_PIXEL_INDEX):
+        pixels[i] = (255, 255, 255)
+        pixels.show()
+        time.sleep(0.03)
+    for i in range(SLASH1_END_PIXEL_INDEX - M_END_PIXEL_INDEX):
+        pixels[i + M_END_PIXEL_INDEX] = (255, 0, 0)
+        pixels.show()
+        time.sleep(0.03)
+    for i in range(SLASH2_END_PIXEL_INDEX - SLASH1_END_PIXEL_INDEX):
+        pixels[i + SLASH1_END_PIXEL_INDEX] = (0, 210, 255)
+        pixels.show()
+        time.sleep(0.03)
+    for i in range(SLASH3_END_PIXEL_INDEX - SLASH2_END_PIXEL_INDEX):
+        pixels[i + SLASH2_END_PIXEL_INDEX] = (0, 0, 255)
+        pixels.show()
+        time.sleep(0.03)
+        
 bmw_colors()
 
 while True:    
+    
+    
+    if(LIGHTS_ON):
+        if(MODE == "pulse"):    
+            pulse_animations.animate()
+        if(MODE == "comet"):
+            comet_animations.animate()   
+        if(MODE == "rgb"):
+            rainbow.animate()    
     try:
-        io.loop()
+        io.loop(0.01)
     except (ValueError, RuntimeError, Exception) as e:
         print("Failed to loop MQTT, attempting to reconnect \n", e)
 
-        try:
-            io.reconnect()
-        except(ValueError, RuntimeError, Exception) as e:
-            print("Failed to reconnect to MQTT, reconnecting \n", e)
+
     
-    print("MODE: " + MODE + " ON?: " + str(LIGHTS_ON))
-    # time.sleep(1)
+    # print("MODE: " + MODE + " ON?: " + str(LIGHTS_ON))
